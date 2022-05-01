8c93aab002e0d70f7ef51e3df466e70a13974838
Polina <dunaeva.pa@phystech.edu>
2022-05-01 21:41:28 +0300

    one more function to factories

--- Factories.h
+++ Factories.h
@@ -7,49 +7,56 @@
 
 class Factory{
 public:
-    virtual std::vector<Button*> create_all_big_buttons() = 0;
-    virtual std::vector<Button*> create_all_small_buttons(size_t number) = 0;
+    virtual std::vector<TransportButton*> create_all_transport_buttons() = 0;
+    virtual std::vector<LetterButton*> create_all_letter_buttons(size_t number) = 0;
+    virtual std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) = 0;
     virtual std::vector<Label*> create_all_labels() = 0;
+
 };
 
 
 class MenuFactory: public Factory{
 public:
     MenuFactory() = default;
-    std::vector<Button*> create_all_big_buttons() override;
-    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<TransportButton*> create_all_transport_buttons() override;
+    std::vector<LetterButton*> create_all_letter_buttons(size_t number) override;
+    std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) override;
     std::vector<Label*> create_all_labels() override;
 };
 
 class MorzeFactory: public Factory{
 public:
     MorzeFactory() = default;
-    std::vector<Button*> create_all_big_buttons() override;
-    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<TransportButton*> create_all_transport_buttons() override;
+    std::vector<LetterButton*> create_all_letter_buttons(size_t number) override;
+    std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) override;
     std::vector<Label*> create_all_labels() override;
 };
 
 class LevelListFactory: public Factory{
 public:
     LevelListFactory() = default;
-    std::vector<Button*> create_all_big_buttons() override;
-    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<TransportButton*> create_all_transport_buttons() override;
+    std::vector<LetterButton*> create_all_letter_buttons(size_t number) override;
+    std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) override;
     std::vector<Label*> create_all_labels() override;
 };
 
 class GameProcessFactory: public Factory{
 public:
     GameProcessFactory() = default;
-    std::vector<Button*> create_all_big_buttons() override;
-    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<TransportButton*> create_all_transport_buttons() override;
+    std::vector<LetterButton*> create_all_letter_buttons(size_t number) override;
+    std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) override;
     std::vector<Label*> create_all_labels() override;
 };
 
 class GameCheckedFactory: public Factory{
 public:
     GameCheckedFactory() = default;
-    std::vector<Button*> create_all_big_buttons() override;
-    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<TransportButton*> create_all_transport_buttons() override;
+    std::vector<LetterButton*> create_all_letter_buttons(size_t number) override;
+    std::vector<ToLevelButton*> create_all_tolevellist_buttons(size_t number) override;
     std::vector<Label*> create_all_labels() override;
 
 };
