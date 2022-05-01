ce92673926190fa8bddc484bf4f8b8a1abafca2f
Polina <dunaeva.pa@phystech.edu>
2022-05-01 20:09:46 +0300

    new factories

--- Factories.cpp
+++ Factories.cpp
@@ -1,131 +1,126 @@
 #pragma once
 #include"Factories.h"
-
 #include <iostream>
 
- void MenuFactory::create_everything() {
-    a.WatchMorzeButton = new Button();
-    std::cout<<"*";
-    CreateMorzeButton(WatchMorzeButton);
-    std::cout<<"*";
-    Button* ToLevelListButton = new Button();
-    CreateToLevelListButton(ToLevelListButton);
-    Label* Label0 = new Label();
-    Label* LabelMorze = new Label();
-    CreateLabel(Label0);
-    CreateLabelMorze(LabelMorze);
+//---------------------------------------------------------------------------//
+
+std::vector<Button*> MenuFactory::create_all_big_buttons() {
+    Button* WatchMorzeButton = new TransportButton();
+    Button* ToLevelListButton = new TransportButton();
+    std::vector<Button*> all_buttons;
+    all_buttons.push_back(WatchMorzeButton);
+    all_buttons.push_back(ToLevelListButton);
+    return all_buttons;
+}
+std::vector<Button*> MenuFactory::create_all_small_buttons(size_t number) {
+    std::vector<Button*> all_buttons;
+    return all_buttons;
 }
 
- void MenuFactory::CreateLabel(Label* label) {
-    label->setAll(L"Morze Game", 100, sf::Color{0x240803ff},
-                  300, 150,
-                 800, 200);
-
+std::vector<Label*> MenuFactory::create_all_labels() {
+    Label* LabelTitle = new Label();
+    Label* MorzeLabel = new Label();
+    std::vector<Label*> answer;
+    answer.push_back(LabelTitle);
+    answer.push_back(MorzeLabel);
+    return answer;
 }
 
-void MenuFactory::CreateLabelMorze(Label *label) {
-    label->setAll(L"-- --- .-. --.. .   --. .- -. .", 100, sf::Color{0x170603ff},
-                 130, 300,
-                 800, 200);
+//-------------------------------------------------------------------------//
+
+std::vector<Button*> LevelListFactory::create_all_big_buttons() {
+    Button* BackToMenu = new TransportButton();
+    std::vector<Button*> all_buttons;
+    all_buttons.push_back(BackToMenu);
+    return all_buttons;
 }
 
- void MenuFactory::CreateMorzeButton(Button* MorzeButton) {
-
-    MorzeButton->set_all(L"Открыть\nазбуку\nМорзе", 35, sf::Color::Yellow,
-                        "../button.png", middle, middle, sf::Color::Magenta, 120, 550,
-                        250, 150);
+std::vector<Button *> LevelListFactory::create_all_small_buttons(size_t number) {
+    std::vector<Button*> all_buttons;
+    for(size_t i = 0; i < number; ++i){
+        all_buttons.push_back(new ToLevelButton());
+    }
+    return all_buttons;
 }
 
-void MenuFactory::CreateToLevelListButton(Button* LevelListButton){
-    LevelListButton->set_all(L"Выбор\nУровней", 35, {0, 0, 139, 230},
-                        "../button.png", middle, middle, sf::Color::Yellow, 800, 550,
-                        250, 150);
+std::vector<Label *> LevelListFactory::create_all_labels() {
+    std::vector<Label*> all_labels;
+    return all_labels;
 }
 
-void LevelListFactory::CreateMenuButton(Button* MenuButton) {
-    MenuButton->set_all(L"Вернуться\nк меню", 35, {0, 0, 139, 230},
-                       "../button.png", middle, middle, sf::Color::Green, 480, 650,
-                       240, 100);
+//--------------------------------------------------------------------------//
+
+std::vector<Button*> GameProcessFactory::create_all_big_buttons() {
+    Button* ToLevelList = new TransportButton();
+    Button* CheckButton = new TransportButton();
+    std::vector<Button*> all_buttons;
+    all_buttons.push_back(ToLevelList);
+    all_buttons.push_back(CheckButton);
+    return all_buttons;
 }
 
-void LevelListFactory::CreateToLevelButtons(ToLevelButton* button, int i) {
-        std::wstring complexity = std::to_wstring(i/5+1);
-        std::wstring level = std::to_wstring(i%5+1);
-        complexity+= ' ';
-        complexity+=level;
-        button->set_all(complexity, 30, {0, 0, 139, 230}, "../button.png", middle, middle, sf::Color::Green,
-                                160+(i%5)*170, 80+(i/5)*170, 150, 150);
-        std::cout<<100+(i/5)*200<<" ";
+std::vector<Button*> GameProcessFactory::create_all_small_buttons(size_t number) {
+    std::vector<Button*> all_buttons;
+    for(size_t i = 0; i < number; ++i){
+        all_buttons.push_back(new LetterButton);
+    }
+    return all_buttons;
 }
 
-void GameProcessFactory::CreateToLevelListButton(Button* ToLevelListButton) {
-    ToLevelListButton->set_all(L"Вернуться\nк выбору\nуровней", 35, {0, 0, 139, 230},
-                            "../button.png", middle, middle, sf::Color::Yellow, 120, 550,
-                            250, 150);
+std::vector<Label*> GameProcessFactory::create_all_labels() {
+    Label* LevelText = new Label();
+    std::vector<Label*> all_labels;
+    all_labels.push_back(LevelText);
+    return all_labels;
 }
 
-void GameProcessFactory::CreateCheckButton(Button* CheckButton) {
-    CheckButton->set_all(L"Проверить", 35, {0, 0, 139, 230},
-                            "../button.png", middle, middle, sf::Color::Yellow, 830, 550,
-                            250, 150);
+//----------------------------------------------------------------------------//
+
+std::vector<Button *> MorzeFactory::create_all_big_buttons() {
+    Button* BackToMenu = new TransportButton();
+    Button* ALphabetButton = new TransportButton();
+    std::vector<Button*> all_buttons;
+    all_buttons.push_back(BackToMenu);
+    all_buttons.push_back(ALphabetButton);
+    return all_buttons;
 }
 
-void GameProcessFactory::CreateLabel(Label* label, int level, int complexity) {
-    std::wstring lab;
-    lab = L"Уровень " + std::to_wstring(complexity) + L"." + std::to_wstring(level);
-    label->setAll(lab, 100, sf::Color{0x240803ff},
-                 300, 100,
-                 800, 200);
+std::vector<Button *> MorzeFactory::create_all_small_buttons(size_t number) {
+    std::vector<Button*> all_buttons;
+    return all_buttons;
 }
 
-void GameProcessFactory::CreateLetterButton(LetterButton* letter_button, int x, int y, std::wstring letter) {
-    letter_button->set_all(letter, 35, {0, 0, 139, 230},
-                        "../button.png", middle, middle, sf::Color::Yellow, x, y,
-                        70, 70);
+std::vector<Label*> MorzeFactory::create_all_labels() {
+    std::vector<Label*> all_labels;
+    return all_labels;
 }
 
-void MorzeFactory::CreateMenuButton(Button* menuButton){
-    menuButton->set_all(L"Вернуться\nк меню", 35, {100, 200, 40, 230},
-                       "../button.png", middle, middle, sf::Color::Blue, 480, 650,
-                       240, 100);
-}
-void MorzeFactory::CreateMorzeAlphabet(Button* AlphabetPhoto) {
-    sf::Texture texture;
-    texture.loadFromFile("../Alphabet.jpg");
-    AlphabetPhoto->setTexture(texture);
-    AlphabetPhoto->setPosition({150, 70});
-    AlphabetPhoto->setSize({900, 520});
+//-------------------------------------------------------------------------//
+
+std::vector<Button *> GameCheckedFactory::create_all_big_buttons() {
+    Button* Again = new TransportButton();
+    Button* Next = new TransportButton();
+    Button* ToList = new TransportButton();
+    std::vector<Button*> all_buttons;
+    all_buttons.push_back(Again);
+    all_buttons.push_back(Next);
+    all_buttons.push_back(ToList);
+    return all_buttons;
 }
 
-void GameCheckedFactory::CreateAgainButton(Button* againbutton) {
-    againbutton->set_all(L"Заново", 35, {0, 0, 139, 230},
-                        "../button.png", middle, middle, sf::Color::Yellow, 120, 550,
-                        250, 150);
+std::vector<Button *> GameCheckedFactory::create_all_small_buttons(size_t number) {
+    std::vector<Button*> all_buttons;
+    for(int i = 0; i < number; ++i){
+        all_buttons.push_back(new LetterButton());
+    }
+    return all_buttons;
 }
 
-void GameCheckedFactory::CreateNextLevelButton(Button* nextbutton) {
-    nextbutton->set_all(L"Следующий\nуровень", 35, {0, 0, 139, 230},
-                        "../button.png", middle, middle, sf::Color::Yellow, 830, 550,
-                        250, 150);
+std::vector<Label *> GameCheckedFactory::create_all_labels() {
+    Label* LevelText = new Label();
+    std::vector<Label*> all_labels;
+    all_labels.push_back(LevelText);
+    return all_labels;
 }
 
-void GameCheckedFactory::CreateToLevelListButton(Button* tolevellistbutton) {
-    tolevellistbutton->set_all(L"К выбору\nуровней", 35, {0, 0, 139, 230},
-                        "../button.png", middle, middle, sf::Color::Yellow, 500, 550,
-                        250, 150);
-}
-
-void::GameCheckedFactory::CreateLetterButton(Button* letter_button, int x, int y, std::wstring& letter){
-    letter_button->set_all(letter, 35, {0, 0, 139, 230},
-                          "../button.png", middle, middle, sf::Color::Yellow, x, y,
-                          70, 70);
-    letter_button->colorChange = false;
-}
-
-void GameCheckedFactory::CreateLabel(Label* label, int level, int complexity) {
-    std::wstring lab;
-    lab = L"Уровень " + std::to_wstring(complexity) + L"." + std::to_wstring(level);
-    label->setAll(lab, 100, sf::Color{0x240803ff},
-                 300, 100,
-                 800, 200);
-}
+//--------------------------------------------------------------------------//
--- Factories.h
+++ Factories.h
@@ -1,70 +1,55 @@
 #pragma once
+#include<vector>
 #include "Buttons.h"
 #include "Label.h"
 #include<string>
 #include "Windows.h"
 
 class Factory{
-    //таких кнопок на странице не больше одной, так что координаты не нужны
-    //все будет сразу прописано в дочерней фабрике
 public:
-    virtual void create_everything() =0;
-    
-
-//    virtual void CreateMenuButton(Button*) = 0;
-//
-//    virtual void CreateMorzeButton(Button *) =0;
-//
-//    virtual void CreateToLevelListButton(Button*) = 0;
-//
-//    virtual void CreateCheckButton(Button*) = 0;
-//
-//    virtual void CreateNextLevelButton(Button*) =0;
-//
-//    virtual void CreateAgainButton(Button*) =0;
-
-    //этих кнопок на странице много - нужны координаты
-//    virtual void CreateToLevelButtons(ToLevelButton*, int i) = 0;
-//    virtual void CreateLetterButton(Button*, int x, int y, std::wstring*) =0;
+    virtual std::vector<Button*> create_all_big_buttons() = 0;
+    virtual std::vector<Button*> create_all_small_buttons(size_t number) = 0;
+    virtual std::vector<Label*> create_all_labels() = 0;
 };
 
 
 class MenuFactory: public Factory{
 public:
     MenuFactory() = default;
-    std::tuple< create_everything() override;
-    void CreateMorzeButton(Button*);//override;
-    void CreateToLevelListButton(Button*);// override;
-    void CreateLabel(Label*);
-    void CreateLabelMorze(Label*);
+    std::vector<Button*> create_all_big_buttons() override;
+    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<Label*> create_all_labels() override;
 };
 
 class MorzeFactory: public Factory{
 public:
-    void CreateMenuButton(Button*);//override;
-    void CreateMorzeAlphabet(Button*);
+    MorzeFactory() = default;
+    std::vector<Button*> create_all_big_buttons() override;
+    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<Label*> create_all_labels() override;
 };
 
 class LevelListFactory: public Factory{
 public:
-    void CreateMenuButton(Button*);//override;
-    void CreateToLevelButtons(ToLevelButton*, int i);//override;
+    LevelListFactory() = default;
+    std::vector<Button*> create_all_big_buttons() override;
+    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<Label*> create_all_labels() override;
 };
 
 class GameProcessFactory: public Factory{
 public:
-    void CreateLabel(Label*, int level, int complexity);
-    void CreateToLevelListButton(Button*);// override;
-    void CreateCheckButton(Button*) ;//override;
-    void CreateLetterButton(LetterButton*, int x, int y, std::wstring letter);
+    GameProcessFactory() = default;
+    std::vector<Button*> create_all_big_buttons() override;
+    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<Label*> create_all_labels() override;
 };
 
 class GameCheckedFactory: public Factory{
 public:
-    void CreateToLevelListButton(Button*) ;//override;
-    void CreateNextLevelButton(Button*);//override;
-    void CreateAgainButton(Button*);//override;
-    void CreateLetterButton(Button*, int x, int y, std::wstring& letter);
-    void CreateLabel(Label*, int level, int complexity);
+    GameCheckedFactory() = default;
+    std::vector<Button*> create_all_big_buttons() override;
+    std::vector<Button*> create_all_small_buttons(size_t number) override;
+    std::vector<Label*> create_all_labels() override;
 
 };
