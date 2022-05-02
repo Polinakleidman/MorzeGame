dc6d068ce2ae06be26dfc34921e4bf6e364a59f9
Polina <dunaeva.pa@phystech.edu>
2022-05-02 16:05:04 +0300

    update levels 1 and 3

---
+++ Levels/Complexity2.cpp
@@ -0,0 +1,10 @@
+Take it easy
+T|.-|-.-|e| |..|t| |.|a|...|y|
+You should work hard
+Y|---|u| |s|....|o|u|.-..|d| |.--|o|.-.|k| |h|a|r|d|
+Do not zone out!
+D|o| |-.|o|-| |--..|o|-.|e| |o|u|t|
+Do not give up!
+D|o| |n|---|t| |--.|i|...-|e| |u|p|
+Be your best version
+B|e| |y|o|u|.-.| |-...|e|...|t| |v|.|r|s|..|o|n|
